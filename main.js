08d082d8e76e8ce67f0beae2bab3e60d155672fe
leoxie080808 <43616278+leoxie080808@users.noreply.github.com>
2020-08-04 21:44:27 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//inits 
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
@@ -6,6 +7,7 @@ const prefix = '-';
 
 const fs = require('fs');
 const { createInterface } = require('readline');
+
 
 client.commands = new Discord.Collection();
 
@@ -16,24 +18,70 @@ for(const file of commandFiles){
     client.commands.set(command.name, command);
 }
 
-
-
-
-
+//turns bot online 
 client.once('ready', () => {
     console.log('util is online');
 });
 
+
+//attachment download
+const http = require('https');
+const https = require('https');
+
+const request = require('request');
+//create an empty file where the data can be saved 
+let file = fs.createWriteStream('file.jpg');
+
+/*await new Promise((resolve, reject)) => {
+    let stream = request({
+        
+    })
+}
+*/
+
+
+
+
+
+/*
+var download = function(url, dest){
+    var file = fs.createWriteStream(dest);
+    var request = http.get(url, function(response){
+        response.pipe(file);
+        file.on('finish', function(){
+            file.close(cb);
+        });
+    }).on('error', function(err){
+       fs.unlink(est);
+
+    });
+};
+*/
+
+
+
+
+
+
+
+
+//here, when util is online
 client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+   // if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
     
+    var Attachment = (message.attachments).array();
+    console.log(Attachment[0].url);
+
+    download(Attachment[0].url, "test.jpeg");
+
+
     if(command === 'ping'){
        createInterface.commands.get('ping').execute(message, args);
     } else if (command == 'youtube'){
-        message.channel.send('https://www.youtube.com/codelyon');
+        //message.channel.send();
     }
 });
 
@@ -45,4 +93,4 @@ client.on('message', message =>{
 
 
 
-client.login('NzM5NzA4NzI1NTM0OTE2NzI4.XyeZbw.2G5qItJyJAE436LxqxHh_uTSCOM');
+client.login('NzM5NzA4NzI1NTM0OTE2NzI4.XyeZbw.RrySc71F04PT22GD3_ySLsZkWH4');
